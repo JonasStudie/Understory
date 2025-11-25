4d16f8dbc15f00eede9e63d35749c8608f595f80
Jonaslowen <jonas.lowen@gmail.com>
2025-11-25 19:28:04 +0100

    a
    a

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express123' });
 });
 
+/* GET phone page. */
+router.get('/phone', function(req, res, next) {
+  res.render('phone', { title: 'Phone Page' });
+});
+
+/* GET mail page. */
+router.get('/mail', function(req, res, next) {
+  res.render('mail', { title: 'Mail Page' });
+});
+
 module.exports = router;
