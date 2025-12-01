0c7d8ae1f412fd6d86f90844be42879bb5db04a7
Jonaslowen <jonas.lowen@gmail.com>
2025-12-01 13:46:47 +0100

    twiliooooo

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var twilio = require('twilio');
+require('dotenv').config();
+
+// Twilio credentials from environment variables
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const twilioPhoneNumber = process.env.TWILIO_PHONE_NUMBER;
+
+const client = twilio(accountSid, authToken);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -16,6 +25,27 @@ router.get('/mail', function(req, res, next) {
   res.render('mail', { title: 'Mail Page' });
 });
 
+/* POST send message via Twilio */
+router.post('/send-message', function(req, res, next) {
+  const { name, phone } = req.body;
+  
+  if (!name || !phone) {
+    return res.status(400).send('Name and phone number are required');
+  }
 
+  client.messages
+    .create({
+      body: `Hej ${name}, velkommen til Understory! Vi er glade for at have dig med. Klik på linket for at komme i gang: `,
+      from: twilioPhoneNumber,
+      to: phone
+    })
+    .then(message => {
+      res.render('success', { phoneNumber: phone });
+    })
+    .catch(error => {
+      console.error('Error sending message:', error);
+      res.status(500).send('Error sending message: ' + error.message);
+    });
+});
 
 module.exports = router;
