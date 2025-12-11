38242aeb4031d27dd16d1cd1607606dd03cef081
OliverDitlev <oldi24ab@student.cbs.dk>
2025-12-11 18:53:44 +0100

    små ændirnger

--- routes/auth.js
+++ routes/auth.js
@@ -22,7 +22,7 @@ function dbRun(sql, params = []) {
   return new Promise((resolve, reject) => {
     db.run(sql, params, function (err) {
       if (err) return reject(err);
-      resolve(this); // this.lastID, this.changes hvis du skal bruge det
+      resolve(this); 
     });
   });
 }
